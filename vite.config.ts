f107df104d12001ca0d6b40b997bac9e7d9f427f
Marmun544 <kovadario628@gmail.com>
2026-02-25 10:14:43 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,26 +1,14 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
-import { componentTagger } from "lovable-tagger";
 
-export default defineConfig(({ mode }) => ({
-  server: {
-    host: "::",
-    port: 8080,
-    hmr: {
-      overlay: false,
-    },
-  },
-  plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
+export default defineConfig({
+  // Ključno za GitHub Pages da bi ikonica radila
+  base: "/maskote-and-fun/", 
+  plugins: [react()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
-  build: {
-    outDir: "dist",   // ovo govori Vercelu gdje je build
-    emptyOutDir: true,
-    sourcemap: false,
-  },
-  base: "./",         // ključno za SPA i custom domenu
-}));
+});
